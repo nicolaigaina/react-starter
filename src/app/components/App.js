6ee5be23470f9cf7feafba63bbbf9a56e460a9e0
Nicolai <nicolaigaina@gmail.com>
2017-10-02 00:44:42 -0700

    changed some startup files.

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -6,14 +6,8 @@ export class App extends React.Component {
     return (
       <div className="container">
         <div className="header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React Starter Kit</h2>
-          <iframe src="https://ghbtns.com/github-btn.html?user=HosseinKarami&repo=React-Starter-Kit&type=star&count=true" frameborder="0" scrolling="0" width="90px" height="20px"></iframe>
-          <iframe src="https://ghbtns.com/github-btn.html?user=HosseinKarami&repo=React-Starter-Kit&type=fork&count=true" frameborder="0" scrolling="0" width="90px" height="20px"></iframe>
+         
        </div>
-        <p className="App-intro">
-          To get started, Fork it and then edit <code>src/components/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
